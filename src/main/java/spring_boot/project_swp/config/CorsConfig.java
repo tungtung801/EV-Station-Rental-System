73b75481a3d8fba485a0c8b889ffe96a673370e8
GIATHE\acer <230933186+Bach-Ho-Gia-The@users.noreply.github.com>
2025-10-01 22:14:39 +0700

    api with frontend

---
+++ src/main/java/spring_boot/project_swp/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package spring_boot.project_swp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Add the specific frontend URL your friend is using
+        config.addAllowedOrigin("http://localhost:5175");
+
+        config.addAllowedOrigin("https://kesha-subartesian-bowen.ngrok-free.dev/");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
